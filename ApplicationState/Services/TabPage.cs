ed8d8a222d5a7e9a3c726962574b2de776c3b45f
Carl Franklin <carl@franklins.net>
2021-10-06 13:32:08 -0400

    Add project files.

---
+++ ApplicationState/Services/TabPage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApplicationState.Services
+{
+    // Each button in the toolbar is rendered with this data
+    public class TabPage
+    {
+        public string Text { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ ApplicationState/Services/ToolbarState.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using ApplicationState.Shared;
+
+namespace ApplicationState.Services
+{
+    public class ToolbarState 
+    {
+        // All the toolbar items are locaed here.
+        public List<TabPage> TabPages { get; set; } = new List<TabPage>();
+
+        // Call this from any page or component that wants to swap one TabPage for another
+        public void UpdateTabPage(ComponentBase Source, TabPage ExistingTabPage, TabPage NewTabPage)
+        {
+            // We need the index in order to update it.
+            var found = (from x in TabPages
+                         where x.Text == ExistingTabPage.Text
+                         && x.Value == ExistingTabPage.Value
+                         select x).FirstOrDefault();
+            if (found != null)
+            {
+                // Get the index
+                var index = TabPages.IndexOf(found);
+                // Update the TabPage in the List
+                TabPages[index] = NewTabPage;
+                // Notify subscribers that this has changed
+                NotifyTabChanged(Source, ExistingTabPage, NewTabPage);
+            }
+        }
+
+        // This will be called from the toolbar when someone clicks on a button
+        public void SendTabClicked(ComponentBase Source, TabPage TabPage)
+        {
+            // Notify subscribers that a button has been clicked on
+            NotifyTabClicked(Source, TabPage);
+        }
+                
+        public event Action<ComponentBase, TabPage> TabPageClicked;
+        private void NotifyTabClicked(ComponentBase Source, TabPage TabPage)
+            => TabPageClicked?.Invoke(Source, TabPage);
+
+        public event Action<ComponentBase, TabPage, TabPage> TabPageUpdated;
+        private void NotifyTabChanged(ComponentBase Source, TabPage ExistingTabPage, TabPage NewTabPage) 
+            => TabPageUpdated?.Invoke(Source, ExistingTabPage, NewTabPage);
+    
+        
+    }
+}
